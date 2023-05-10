7b6500d19f6a6612dd361cc51784f266fc5ba103
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-10 13:47:50 -0500

    fixed the counter and yellow-btn

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(onReady);
 function onReady() {
     $('#generate-btn').on('click', generateHandle);
     $('.div-gen').append('<button>Delete</button><button>Yellow</button>')
-    $('#yellow-btn').on('click',colorChange);
-    $('#delete-btn').on('click',deleteHandle);
+    $('#blockContainer').on('click','#yellow-btn',colorChange);
+    $('#blockContainer').on('click','#delete-btn',deleteHandle);
     
 }
 let divCounter = 1;
@@ -13,7 +13,11 @@ let divCounter = 1;
 function generateHandle() {
     
    
-    $('#blockContainer').append('<div class="div-gen"><p><span class="div-count">1</span></p><button id="delete-btn">Delete</button><button id="yellow-btn">Yellow</button></div>');
+    $('#blockContainer').append(`<div class="div-gen">
+                                <p> ${divCounter}</p>
+                                <button id="delete-btn">Delete</button>
+                                <button id="yellow-btn">Yellow</button>
+                                </div>`);
     divCounter+=1;
     
     $('.div-count').text(divCounter)
@@ -22,8 +26,9 @@ function generateHandle() {
 }
 function colorChange() {
     //$('.div-gen').css('background-color','yellow');
-    $('.div-gen').addClass('yellow-background')
+    $(this).parent().addClass('yellow-background')
 }
 function deleteHandle() {
-    $(this).parent().parent().remove();
+    $(this).parent().remove();
+    divCounter-=1
 }
